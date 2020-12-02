43b3965fed773ec9705f44522be8f3e2d375cd87
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-02 20:29:21 +0000

    Center the bricks and but back-brick on front-brick.

--- js/game.js
+++ js/game.js
@@ -1,4 +1,13 @@
 
+const bricks = document.querySelectorAll('.brick');
+
+function flipCard() {
+    console.log('Clicked!');
+    this.classList('flip');
+}
+
+bricks.forEach(brick => brick.addEventListener('click', flipCard))
+
 /*
 //variables
 var numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
